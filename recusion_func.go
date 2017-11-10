1fbeb7917336f3fffb1e2c3c1c3911cadbf66f3c
ishak dölek <ishak-65@hotmail.com>
2017-11-10 12:00:43 +0300

    add recursion func

---
+++ recusion_func.go
@@ -0,0 +1,15 @@
+package main
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Result:",fact(12))
+}
+
+func fact(n int) int {
+    if n == 0 {
+        return 1
+    }
+    return n * fact(n-1)
+}
