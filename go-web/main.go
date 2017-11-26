45d5281946506dfca9abdaed2e7db9e84657804b
ishak dölek <ishak-65@hotmail.com>
2017-11-26 13:37:01 +0300

    bug fix

--- go-web/main.go
+++ go-web/main.go
@@ -9,13 +9,8 @@ import(
 var client *redis.Client
 var templates *template.Template
 
-//ı will continue..
 func main(){
 	templates = template.Must(template.ParseGlob("templates/*.html"))
- client=redis.NewClient(&redis.Options{
-	 Addr:"localhost:8080",
- })	
-	
 	r:= mux.NewRouter()
 	r.HandleFunc("/",indexHandler).Methods("GET") 
 	http.Handle("/",r) 	
@@ -23,9 +18,5 @@ func main(){
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
-	// comments, err:=client.LRange("comments",0,10).Result()
-	//  if err!=nil{
-	//  	return
-	//  }
 	templates.ExecuteTemplate(w,"index.html",nil)
 }
