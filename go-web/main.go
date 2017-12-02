b3b47ca083aab6f12d82f679107b5c22b8309e99
ishakdolek <ishak-65@hotmail.com>
2017-12-02 16:12:03 +0300

    dressing template to web app.

--- go-web/main.go
+++ go-web/main.go
@@ -4,13 +4,17 @@ import(
 	"github.com/gorilla/mux"
 	"html/template"
 	"github.com/go-redis/redis"
+	"log"
 )
 
 var client *redis.Client
 var templates *template.Template
 
+
 func main(){
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	templates = template.Must(template.ParseGlob("templates/*.html"))
+    // http.Handle("/", http.FileServer(http.Dir("/tmp")))
 	r:= mux.NewRouter()
 	r.HandleFunc("/",indexHandler).Methods("GET") 
 	http.Handle("/",r) 	
@@ -18,5 +22,13 @@ func main(){
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
-	templates.ExecuteTemplate(w,"index.html",nil)
+			renderTemplate(w, "templates/index.html", nil)	
+}
+
+func renderTemplate(w http.ResponseWriter, templateFile string, templateData interface{}) {
+	t, err := template.ParseFiles(templateFile)
+	if err != nil {
+		log.Fatal("Error encountered while parsing the template: ", err)
+	}
+	t.Execute(w, templateData)
 }
